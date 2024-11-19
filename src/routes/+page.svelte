bbec9a83315267b05072b4495285d412be38af06
The-BigMan-tech <saddlebox9@gmail.com>
2024-11-19 13:16:45 +0100

    Finished two sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,10 @@
     import Navbar from "$lib/components/Navbar.svelte";
     import Herosection from "$lib/components/Herosection.svelte";
     import Features from "$lib/components/Features.svelte";
+    import Best from "$lib/components/Best.svelte";
 </script>
 
 <Navbar/>
 <Herosection/>
 <Features/>
+<Best/>
