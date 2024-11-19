c4525adbaed67854f5524f72037bf435ab7cc4cf
The-BigMan-tech <saddlebox9@gmail.com>
2024-11-19 12:07:49 +0100

    A clean update

--- src/lib/components/Herosection.svelte
+++ src/lib/components/Herosection.svelte
@@ -3,13 +3,13 @@
     let hero = content.HeroSection
 </script>
 
-<div class="bg-[#c63451] flex h-[25rem] relative">
+<div class="bg-[#c63451] flex h-[30rem] relative">
     <div class="flex flex-col ml-10 absolute top-32 gap-7">
         <h1 class="font-bold text-white font-space_mono text-5xl leading-[3.5rem] w-[35rem]">{hero.heading}</h1>
         <p class="text-white text-lg font-[Consolas] w-[30rem]">{hero.paragraph}</p>
-        <div class="flex gap-4 bg-white w-36 py-3 px-4 rounded-lg relative bottom-3 shadow-xl">
-            <img class="w-6 " src={hero.icons.buy} alt="">
-            <p class="font-bold">{hero.buy}</p>
+        <div class="flex gap-4 w-36 py-3 px-4 rounded-lg relative bottom-3 shadow-xl bg-[#fb4368]">
+            <img class="w-6" src={hero.icons.buy} alt="">
+            <p class="font-bold font-[Consolas] text-lg text-white">{hero.buy}</p>
         </div>
     </div>
     <div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
     import Navbar from "$lib/components/Navbar.svelte";
     import Herosection from "$lib/components/Herosection.svelte";
+    import Features from "$lib/components/Features.svelte";
 </script>
 
 <Navbar/>
 <Herosection/>
+<Features/>
